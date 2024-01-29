07905defdce0259fc97c6feb01f1890661126af8
VinayaAugusthy <vinayaaugusthy@gmail.com>
2024-01-29 18:28:25 +0530

    theme

--- attendance/lib/presentation/monthly_report/monthly_report.dart
+++ attendance/lib/presentation/monthly_report/monthly_report.dart
@@ -5,6 +5,15 @@ class MonthlyReport extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MonthlyReport'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+          child: Column(
+        children: [],
+      )),
+    );
   }
 }
