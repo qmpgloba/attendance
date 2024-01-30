3525d974a38417fe903583caffb55d1b81e1ebdd
VinayaAugusthy <vinayaaugusthy@gmail.com>
2024-01-30 15:59:28 +0530

    folder rearrangement

--- attendance/lib/main.dart
+++ attendance/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:attendance/presentation/authentication/auth.dart';
+import 'package:attendance/presentation/authentication/login/login.dart';
 
 import 'package:flutter/material.dart';
 
